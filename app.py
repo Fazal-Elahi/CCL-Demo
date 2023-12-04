6dd6a5c497839725062a76cbb2ec15928a027797
Fazal Elahi <103296871+Fazal-Elahi@users.noreply.github.com>
2023-12-04 13:39:12 +0000

    Main
    PostCode Format, Env variable checker & workflow.

--- app.py
+++ app.py
@@ -2,12 +2,23 @@ from database import PostcodeDatabase
 from api_client import GetAddressAPI
 from dotenv import load_dotenv
 import os
+import re
+from datetime import datetime
+
+def uk_postcode_format(postcode):
+    # UK postcode regex pattern
+    pattern = r'^[A-Z]{1,2}[0-9R][0-9A-Z]? ?[0-9][A-Z]{2}$'
+    return re.match(pattern, postcode, re.IGNORECASE)
 
 def main():
     load_dotenv()  # Load environment variables
 
     db_uri = os.getenv('DATABASE_URI')
     api_key = os.getenv('API_KEY')
+
+    if not db_uri or not api_key:
+        print("Database URI or API Key is not set. Please check your environment variables.")
+        return
 
     db = PostcodeDatabase(db_uri)
     api_client = GetAddressAPI(api_key)
@@ -17,15 +28,26 @@ def main():
         if postcode.lower() == 'exit':
             break
 
-        result = db.find_postcode(postcode)
-        if result and (datetime.now() - result['last_accessed']).seconds < 86400:
-            addresses = result['addresses']
-        else:
-            addresses = api_client.get_addresses(postcode)
-            db.update_postcode(postcode, addresses)
+        if not uk_postcode_format(postcode):
+            print("Invalid postcode format. Please enter a valid postcode.")
+            continue
 
-        for address in addresses:
-            print(address)
+        try:
+            result = db.find_postcode(postcode)
+            if result and (datetime.now() - result['last_accessed']).seconds < 86400:
+                addresses = result['addresses']
+            else:
+                addresses = api_client.get_addresses(postcode)
+                if addresses:
+                    db.update_postcode(postcode, addresses)
+                else:
+                    print("Failed to fetch addresses from the API.")
+                    continue
+
+            for address in addresses:
+                print(address)
+        except Exception as e:
+            print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
