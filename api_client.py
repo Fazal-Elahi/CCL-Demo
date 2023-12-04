e8970959b3d7b0dd406f7cbad8262d6d91a7b10a
Fazal Elahi <103296871+Fazal-Elahi@users.noreply.github.com>
2023-12-04 13:38:33 +0000

    API Interface
    Handling Exceptions & Error

--- api_client.py
+++ api_client.py
@@ -1,13 +1,21 @@
 import requests
+from requests.exceptions import RequestException, HTTPError
 
 class GetAddressAPI:
     def __init__(self, api_key):
         self.api_key = api_key
-
     def get_addresses(self, postcode):
-        url = f"https://api.getAddress.io/find/{postcode}?api-key={self.api_key}"
-        response = requests.get(url)
-        if response.status_code == 200:
+        try:
+            url = f"https://api.getAddress.io/find/{postcode}?api-key={self.api_key}"
+            response = requests.get(url, timeout=10)  # Add a timeout
+            response.raise_for_status()  # Check for HTTP errors
             return response.json()['addresses']
-        else:
-            response.raise_for_status()
+        except HTTPError as e:
+            print(f"HTTP error: {e.response.status_code}")
+            return None
+        except RequestException as e:
+            print(f"Request error: {e}")
+            return None
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            return None
